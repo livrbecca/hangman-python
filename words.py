91591a14120c68f366170d78881713fc5e96f98f
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-05-25 13:56:12 +0100

    finished hangman game

---
+++ words.py
@@ -0,0 +1,21 @@
+word_list = [
+    "eastenders",
+    "baileys",
+    "beyonce",
+    "pandemic",
+    "python",
+    "code",
+    "food",
+    "exercise",
+    "sleep",
+    "eat",
+    "flower",
+    "park",
+    "sun",
+    "earth",
+    "orange",
+    "salad",
+    "perfume",
+    "happy",
+    "netflix"
+]
